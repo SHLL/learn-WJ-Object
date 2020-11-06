d90c343175ee4d272cae5b4e207d54a76a7ef3fc
SHLL <1204108226@qq.com>
2020-11-06 20:46:41 +0800

    updata 201106-pm

--- wj-vue-test-1/src/main.js
+++ wj-vue-test-1/src/main.js
@@ -4,6 +4,17 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+//载入iView组件
+import iView from "iview";
+import 'iview/dist/styles/iview.css'
+Vue.use(iView);
+//载入easy
+import 'vue-easytable/libs/themes-base/index.css'
+import {VTable,VPagination} from 'vue-easytable'
+Vue.component(VTable.name,VTable)
+Vue.component(VPagination.name,VPagination)
+
+
 Vue.config.productionTip = false
 
 new Vue({
